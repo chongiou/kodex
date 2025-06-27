4d90087ec1d718850709657592d4027ab0f417d4
重楼 <chongiou@qq.com>
2025-06-27 21:29:34 +0800

    重构: 将 JSX 工厂移动到 jsx-runtime 模块并重构导出结构
    - 将 JSX 工厂实现从 index.ts 移动到 jsx-runtime.ts
    - 简化 index.ts 的导出结构

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,4 @@
-import '@/types/jsx'
-
-type JSXNode = JSX.Element | string | number | boolean | null | undefined
-
-function jsx<T extends keyof JSX.IntrinsicElements | Function>(
-  type: T,
-  props: JSX.IntrinsicElements[T extends keyof JSX.IntrinsicElements ? T : any] & { children?: any }
-) {
-  if (props.children && Array.isArray(props.children)) {
-    props.children = props.children.flat(Infinity) as JSXNode[]
-  }
-  return { type, props } as JSX.Element
-}
-
-const Fragment = (props: any) => props.children
-
-export { jsx, jsx as jsxs, Fragment }
-export * from '@/core'
+export * from '@/jsx-runtime'
+export * from '@/core/renderer'
+export * from '@/core/reactive'
+export * from '@/utils/components'
--- src/jsx-runtime.ts
+++ src/jsx-runtime.ts
@@ -1,1 +1,17 @@
-export { jsx, jsxs, Fragment } from './index'
+import '@/types/jsx'
+
+type JSXNode = JSX.Element | string | number | boolean | null | undefined
+
+function jsx<T extends keyof JSX.IntrinsicElements | Function>(
+  type: T,
+  props: JSX.IntrinsicElements[T extends keyof JSX.IntrinsicElements ? T : any] & { children?: any }
+) {
+  if (props.children && Array.isArray(props.children)) {
+    props.children = props.children.flat(Infinity) as JSXNode[]
+  }
+  return { type, props } as JSX.Element
+}
+
+const Fragment = (props: any) => props.children
+
+export { jsx, jsx as jsxs, Fragment }
